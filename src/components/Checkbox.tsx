8a846a6648de94a5466588a6ad35c9c19c2fe001
mariafantuci <mariafantuci@gmail.com>
2022-10-12 20:06:08 -0300

    Fixing the style whe there's no item and when there're some

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,9 +5,9 @@ export interface CheckboxProps {}
 
 export function Checkbox ({ }: CheckboxProps) {
     return (
-        <CheckboxPrimitive.Root className="w-6 h-6 p-0.5 bg-gray-800 rounded">
+        <CheckboxPrimitive.Root className="min-w-[24px] w-6 h-6 p-0.5 border border-blue rounded-full checkbox_task">
             <CheckboxPrimitive.Indicator asChild >
-                <Check weight='bold' className='w-5 h-5 text-cyan-500' />
+                <Check weight='bold' className='w-5 h-5 text-gray-100' />
             </CheckboxPrimitive.Indicator>
         </CheckboxPrimitive.Root>
     )
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,14 +1,17 @@
-import { Clipboard } from 'phosphor-react'
+import { Clipboard, Trash, PencilSimple } from 'phosphor-react'
+import { Checkbox } from './Checkbox'
+
+
 export function TaskList(){
     return (
         <main className="mt-16">
             <header className="flex items-center justify-between">
                 <div className="flex items-center gap-2">
-                    <h2 className="text-blue text-xs">Tarefas Criadas</h2>
+                    <h2 className="text-blue text-xs font-bold">Tarefas Criadas</h2>
                     <span className="bg-gray-400 gap-2.5 rounded-[999px] text-gray-200 py-0.5 px-2">0</span>
                 </div>
                 <div className="flex items-center gap-2">
-                    <h2 className="text-purple text-xs">Concluídas</h2>
+                    <h2 className="text-purple text-xs font-bold">Concluídas</h2>
                     <span className="bg-gray-400 gap-2.5 rounded-[999px] text-gray-200 py-0.5 px-2">0</span>
                 </div>
             </header>
@@ -20,7 +23,28 @@ export function TaskList(){
                         <p className="mt-1 text-gray-300">Crie tarefas e organize seus itens a fazer</p>
                     </div>
                 </div>
-
+                <div className="flex w-full flex-col gap-3">
+                    <div className="border bg-gray-500 border-gray-400 rounded flex flex-row items-center justify-between p-4">
+                        <div className="gap-3 flex items-center w-[90%]">
+                            <Checkbox />
+                            <p className="text-gray-100 text-xs">Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                        </div>
+                        <div className="flex items-end gap-2 w-[10%]">
+                            <Trash size={24} className="text-gray-300 hover:text-danger focus:text-danger"/>
+                            <PencilSimple size={24} className="text-gray-300 hover:text-gray-100 focus:text-gray-100"/>
+                        </div>
+                    </div>
+                    <div className="border bg-gray-500 border-gray-400 rounded flex flex-row items-center justify-between p-4 line-through decoration-gray-300">
+                        <div className="gap-3 flex items-center w-[90%]">
+                            <Checkbox />
+                            <p className="text-gray-300 text-xs">Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                        </div>
+                        <div className="flex items-end gap-2 w-[10%]">
+                            <Trash size={24} className="text-gray-300 hover:text-danger focus:text-danger"/>
+                            <PencilSimple size={24} className="text-gray-300 hover:text-gray-100 focus:text-gray-100"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
