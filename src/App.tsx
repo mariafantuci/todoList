226d165c485975e983c9758befee0dd256603113
mariafantuci <mariafantuci@gmail.com>
2022-10-12 17:16:39 -0300

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './styles/global.css'
 
-export function App() {
-  return (<h1>Ola mundo</h1>);
+export function App() {   
+  return (<h1 className="bg-gray-900 text-gray-100"> Ola mundo </h1>);
 }
